174ddcc1c36891d416baa7aa210952f24794a8a8
Gedare Bloom <gedare@rtems.org>
2021-04-12 18:46:09 -0600

    add list.h

---
+++ list.h
@@ -0,0 +1,86 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+
+/**
+ * @file
+ *
+ * @ingroup RTEMSCChallenge
+ *
+ * @brief C Programming Challenging for RTEMS 
+ *
+ * Definition of List Interface for RTEMS C Challenge.
+ */
+
+/*
+ * Copyright (C) 2021 Gedare Bloom
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _LIST_H
+#define _LIST_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct list list;
+
+/* Allocates and initializes a new list. */
+list* create_list(void);
+
+/* Adds item to end of the list. This function allocates a
+ * new buffer and copies the string from item (use malloc,
+ * strlen, and strncpy; or try strdup).
+ * Returns 0 if successful, non-zero otherwise. */
+int add_to_list(list* ll, char* item);
+
+/* Removes the string from the front of the list and
+ * returns a pointer to it. The caller is expected to free
+ * the string returned when finished with it. Returns NULL
+ * if the list is empty. */
+char* remove_from_list(list* ll);
+
+/* Prints every string in the list, with a new line
+ * character at the end of each string. */
+void print_list(list *ll);
+
+/* Flushes (clears) the entire list and re-initializes the
+ * list. The passed pointer ll should still point to a
+ * valid, empty list when this function returns. Any memory
+ * allocated to store items in the list should be freed.
+ * Returns the number of items flushed from the list.
+ * Note: after flush_list() the list can still be used, 
+ * just as if it has just been created.*/
+int flush_list(list* ll);
+
+/* De-allocates all data for the list. Ensure all memory
+ * allocated for this list is freed, including any
+ * allocated strings and the list itself.
+ * Note: after free_list() it is invalid to still use the
+ * list pointer. */
+void free_list(list *ll);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _LIST_H */
